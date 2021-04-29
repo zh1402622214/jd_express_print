7dd91ba1585fb75d7b804a972439fb2afc1df3b1
Sanxc <1402622214@qq.com>
2021-04-29 23:51:21 +0800

    修复功能: 获取打印数据失败

--- src/Lop/Api/Request/LCP_00072403/PullDataServicePullDataLopRequest.php
+++ src/Lop/Api/Request/LCP_00072403/PullDataServicePullDataLopRequest.php
@@ -36,7 +36,7 @@ public function getApiMethod(){
 
 public function getAppJsonParams() {
     $apiParams = array();
-        $apiParams[$i] = $this->pullDataReqDTO;
+        $apiParams[] = $this->pullDataReqDTO;
     return json_encode($apiParams);
 }
 public function getDomain(){
@@ -44,7 +44,7 @@ public function getDomain(){
 }
 public function getBodyObject(){
     $apiParams = array();
-        $apiParams[$i] = $this->pullDataReqDTO;
+        $apiParams[] = $this->pullDataReqDTO;
     return $apiParams;
 }
 public function buildDomainHttpParam(LopDomainClient $client)
